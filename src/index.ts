061f9432b9a1ba38143a2a29e1b35215322a333b
Andrés Castañeda <ac192429@gmail.com>
2023-10-21 22:04:35 -0500

    changes file name and nodemon added to package

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,10 @@ const port = process.env.PORT || 3000;
 const uridb = process.env.MONGODB_URI || "mongodb://localhost:27017/dna_anomalies";
 // Configuración de conexión a la base de datos
 mongoose.connect(`${uridb}`, { useNewUrlParser: true, useUnifiedTopology: true } as any);
+
+app.get("/", (req, res) => {
+    res.send("<h1>Bienvenido a la API de detección de anomalías en el ADN</h1><br> Las rutas disponibles son:<br>POST /api/validate-anomaly<br>GET /api/stats");
+});
 app.use("/api", dnaRoutes);
 
 app.listen(port, () => {
